027b9ebeec83632fe36ae57a65ced823bec7865d
sreyash8580 <78336924+sreyash8580@users.noreply.github.com>
2021-10-26 18:49:08 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,90 @@
+
+// function ten(){
+//     console.log("10")
+// }
+// setTimeout(ten,1000)
+
+// function nine(){
+//     console.log("9")
+// }
+// setTimeout(nine,2000)
+
+// function eight(){
+//     console.log("8")
+// }
+// setTimeout(eight,3000)
+
+// function seven(){
+//     console.log("7")
+// }
+// setTimeout(seven,4000)
+
+// function six(){
+//     console.log("6")
+// }
+// setTimeout(six,5000)
+
+// function five(){
+//     console.log("5")
+// }
+// setTimeout(five,6000)
+
+// function four(){
+//     console.log("4")
+// }
+// setTimeout(four,7000)
+
+// function three(){
+//     console.log("3")
+// }
+// setTimeout(three,8000)
+
+// function two(){
+//     console.log("2")
+// }
+// setTimeout(two,9000)
+
+// function one(){
+//     console.log("1")
+// }
+// setTimeout(one,10000)
+
+// function HBD(){
+//     console.log("Happy Birth Day")
+// }
+// setTimeout(HBD,11000)
+
+
+setTimeout(function(){
+    console.log("10")
+},1000)
+setTimeout(function(){
+    console.log("9")
+},2000)
+setTimeout(function(){
+    console.log("8")
+},3000)
+setTimeout(function(){
+    console.log("7")
+},4000)
+setTimeout(function(){
+    console.log("6")
+},5000)
+setTimeout(function(){
+    console.log("5")
+},6000)
+setTimeout(function(){
+    console.log("4")
+},7000)
+setTimeout(function(){
+    console.log("3")
+},8000)
+setTimeout(function(){
+    console.log("2")
+},9000)
+setTimeout(function(){
+    console.log("1")
+},10000)
+setTimeout(function(){
+    console.log("Happy Birth Day")
+},11000)
